05c7da58a27ed853a7e07466609ec93c8fb7438d
Majid Arif Siddiqui <me@majidarif.com>
2014-10-06 14:16:37 +0800

    updated

--- test/repo.js
+++ test/repo.js
@@ -3,6 +3,7 @@
 var should = require('should');
 var chance = require('chance').Chance();
 var cassandra = require('cassandra-driver');
+var Model = require('../lib/model');
 
 var Repo = require('../');
 
@@ -28,6 +29,8 @@ var db = new Repo({
 });
 
 describe('Repo', function () {
+  var TransactionSchema;
+
   describe('#init', function () {
     it('should load all enabled adapters', function (done) {
       db.init(done);
@@ -35,8 +38,8 @@ describe('Repo', function () {
   });
 
   describe('#schema', function () {
-    it('should return a model', function (done) {
-      var Transaction = db.schema('transaction_logs', {
+    it('should return a schema', function (done) {
+      var schema = {
         channel_id:       { type: 'text',     required: true },
         date:             { type: 'text',     required: true },
         event_id:         { type: 'timeuuid', required: true },
@@ -48,45 +51,25 @@ describe('Repo', function () {
         recipient_id:     { type: 'text',     required: true },
         data_id:          { type: 'text',     required: true },
         event_status:     { type: 'text',     required: true }
-      }, {
-        mysql: {
-          table: 'txl'
-        }
-      });
+      };
 
-      Transaction.should.be.an.instanceof(Object);
+      TransactionSchema = db.schema('transaction_logs', schema);
+
+      TransactionSchema.should.eql(schema);
+
       done();
     });
   });
 
-  describe('#insert', function () {
-    it('should insert to all enabled adapters', function (done) {
-      var transaction = {
-        channel_id:       chance.word(),
-        date:             chance.date({string: true}),
-        event_id:         cassandra.types.timeuuid(),
-        transaction_id:   cassandra.types.uuid(),
-        source_system_id: chance.word(),
-        target_system_id: chance.word(),
-        subject_id:       chance.word(),
-        sender_id:        chance.word(),
-        recipient_id:     chance.word(),
-        data_id:          chance.word(),
-        event_status:     chance.word()
-      }
+  describe('#model', function () {
+    it('should return a model', function (done) {
+      var Transaction = db.model('Transaction', [TransactionSchema]);
 
-      var options = {
-        /**
-        amqp: {
-          key: 'export',
-          exchange: 'caresharing.medintegrate'
-        }
-        **/
-      };
-
-      db.insert('transaction_logs', transaction, options, done);
+      Transaction.should.be.an.instanceof(Model);
+      done();
     });
   });
+
 });
 
 describe('Model', function () {
