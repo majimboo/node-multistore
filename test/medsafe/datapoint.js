31f2df4706cebbc03b498cd0d2146bf2d00cb654
Majid Arif Siddiqui <me@majidarif.com>
2014-10-07 19:50:29 +0800

    trying to fix problem on travis

--- test/medsafe/datapoint.js
+++ test/medsafe/datapoint.js
@@ -11,7 +11,7 @@ describe('DataPoint', function () {
     repo.init({
       cassandra: {
         on: 'create',
-        contactPoints: ['127.0.0.1'],
+        contactPoints: ['localhost'],
         keyspace: 'medsafe'
       }
     }, function (err) {
